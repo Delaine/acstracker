d1dc20acfab9e346b7aa26ea736713b14eb36d97
Delaine Lawrence <delaine.lawrence35@gmail.com>
2015-08-07 18:26:26 -0400

    form-based workflow feature completed

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,5 @@
 class JobsController < ApplicationController
+	include JobsHelper
 	
 	def index
 		@jobs = Job.all
@@ -6,5 +7,40 @@ class JobsController < ApplicationController
 
 	def show
 		@job = Job.find(params[:id])
+	end
+
+	def new
+		@job = Job.new
+	end
+
+	def create
+		@job = Job.new(job_params)
+  		@job.save
+
+  		flash.notice = "Job '#{@job.name}' Created!"
+
+  		redirect_to job_path(@job)
+	end
+
+	def destroy
+		@job = Job.find(params[:id])
+		@job.destroy
+
+		flash.notice = "Job '#{@job.name}' Deleted!"
+
+		redirect_to jobs_path
+	end
+
+	def edit
+		@job = Job.find(params[:id])
+	end
+
+	def update
+		@job = Job.find(params[:id])
+		@job.update(job_params)
+
+		flash.notice = "Job '#{@job.name}' Updated!"
+
+		redirect_to job_path(@job)
 	end
 end
--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,2 +1,7 @@
 module JobsHelper
+
+	def job_params
+		params.require(:job).permit(:name, :tech_number, :account_number, :points, :date, :finding_code)
+	end
+
 end
